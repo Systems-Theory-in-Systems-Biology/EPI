243eb47f63d0450fb8c8a52ae3ed86fe6dfa6e01
Lars Kaiser <lars.g.kaiser@gmx.de>
2023-02-03 21:26:27 +0100

    Add doc string to jax_extension

--- epi/jax_extension.py
+++ epi/jax_extension.py
@@ -6,6 +6,12 @@ import jax.numpy as jnp
 
 
 def value_and_jacfwd(fun: Callable[[jnp.ndarray], jnp.ndarray]):
+    """Returns a function that computes the value and the jacobian of the passed function using forward mode AD.
+
+    :param fun: The function to supplement with the jacobian
+    :type fun: Callable[[jnp.ndarray], jnp.ndarray]
+    """
+
     def value_and_jacfwd_fun(x: jnp.ndarray):
         pushfwd = partial(jax.jvp, fun, (x,))
         y, jac = jax.vmap(pushfwd, out_axes=(None, -1))(
@@ -17,6 +23,12 @@ def value_and_jacfwd(fun: Callable[[jnp.ndarray], jnp.ndarray]):
 
 
 def value_and_jacrev(fun: Callable[..., jnp.ndarray]):
+    """Returns a function that computes the value and the jacobian of the passed function using reverse mode AD.
+
+    :param fun: The function to supplement with the jacobian
+    :type fun: Callable[..., jnp.ndarray]
+    """
+
     def value_and_jacrev_fun(x):
         y, pullback = jax.vjp(fun, x)
         jac = jax.vmap(pullback)(jnp.eye(y.shape[0], dtype=y.dtype))[0]
