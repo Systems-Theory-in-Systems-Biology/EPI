289036876ff06ac9bf8e0fb6ca32c01e3d08c122
deckard007 <63878559+castellinilinguini@users.noreply.github.com>
2023-03-06 18:08:47 +0100

    Updated results manager to store model_name and run_name
    Also fixed several bugs and changed folder structure to support multiple runs

--- epi/core/result_manager.py
+++ epi/core/result_manager.py
@@ -8,14 +8,18 @@ import seedir
 from seedir import FakeDir, FakeFile
 
 from epi import logger
+from epi.core.model import Model
 
 
 class ResultManager:
-    @staticmethod
-    def countEmceeSubRuns(model, slice: np.ndarray) -> int:
+    def __init__(self, model_name, run_name) -> None:
+        # TODO comment
+        self.model_name = model_name
+        self.run_name = run_name
+
+    def countEmceeSubRuns(slice: np.ndarray) -> int:
         """This data organization function counts how many sub runs are saved for the specified scenario.
 
-        :param model: The model for which the files will be counted
         :param slice: the slice for which the files will be counted
         :return: numExistingFiles (number of completed sub runs of the emcee particle swarm sampler)
         """
@@ -24,7 +28,7 @@ class ResultManager:
 
         # Increase the just defined number until no corresponding file is found anymore ...
         while path.isfile(
-            ResultManager.getSlicePath(model, slice)
+            ResultManager.getSlicePath(slice)
             + "/DensityEvals/"
             + str(numExistingFiles)
             + ".csv"
@@ -33,7 +37,6 @@ class ResultManager:
 
         return numExistingFiles
 
-    @staticmethod
     def getSliceName(slice: np.ndarray) -> str:
         """This organization function returns the name of the folder for the current slice.
 
@@ -43,7 +46,6 @@ class ResultManager:
 
         return "Slice_" + "Q".join([str(i) for i in slice])
 
-    @staticmethod
     def getSlicePath(self, slice: np.ndarray) -> str:
         """Returns the path to the folder where the results for the given slice are stored.
 
@@ -51,11 +53,12 @@ class ResultManager:
         :return: The path to the folder where the results for the given slice are stored.
         """
         sliceName = ResultManager.getSliceName(slice)
-        return os.path.join("Applications", self.name, sliceName)
+        return os.path.join(
+            "Applications", self.model_name, self.run_name, sliceName
+        )
 
-    @staticmethod
     def createApplicationFolderStructure(
-        model, slices: list[np.ndarray] = None
+        model: Model, slices: list[np.ndarray] = None
     ) -> None:
         """Creates the `Application` folder including subfolder where all simulation results
         are stored for this model. No files are deleted during this action.
@@ -69,8 +72,7 @@ class ResultManager:
         for slice in slices:
             ResultManager.createSliceFolderStructure(model, slice)
 
-    @staticmethod
-    def createSliceFolderStructure(model, slice: np.ndarray) -> None:
+    def createSliceFolderStructure(self, slice: np.ndarray) -> None:
         """Creates the subfolders in `Aplication` for the given slice where all simulation results
         are stored for this model and slice. No files are deleted during this action.
 
@@ -80,10 +82,11 @@ class ResultManager:
         applicationFolderStructure = (
             "Applications/ \n"
             "  - {modelName}/ \n"
-            "    - {sliceName}/ \n"
-            "       - DensityEvals/ \n"
-            "       - Params/ \n"
-            "       - SimResults/ \n"
+            "    - {runName}/ \n"
+            "       - {sliceName}/ \n"
+            "           - DensityEvals/ \n"
+            "           - Params/ \n"
+            "           - SimResults/ \n"
         )
         path = "."
         structure = applicationFolderStructure
@@ -105,50 +108,54 @@ class ResultManager:
 
         sliceName = ResultManager.getSliceName(slice)
         fakeStructure = seedir.fakedir_fromstring(
-            structure.format(modelName=model.name, sliceName=sliceName)
+            structure.format(
+                modelName=self.model_name,
+                runName=self.run_name,
+                sliceName=sliceName,
+            )
         )
         fakeStructure.realize = lambda path_arg: fakeStructure.walk_apply(
             create, root=path_arg
         )
         fakeStructure.realize(path)
 
-    @staticmethod
-    def deleteApplicationFolderStructure(model, slices) -> None:
-        """Deletes the models `Applications` subfolder
+    def deleteApplicationFolderStructure(slices) -> None:
+        """Deletes the `Applications` subfolder
 
-        :param model: The model for which the folder structure will be deleted
         :param slices: The slices for which the folder structure will be deleted
         """
         for slice in slices:
             try:
-                ResultManager.deleteSliceFolderStructure(model, slice)
+                ResultManager.deleteSliceFolderStructure(slice)
             except FileNotFoundError:
                 logger.info(
                     f"Folder structure for slice {slice} does not exist"
                 )
 
-    @staticmethod
-    def deleteSliceFolderStructure(model, slice: np.ndarray) -> None:
-        """Deletes the models `Applications` subfolder
+    def deleteSliceFolderStructure(slice: np.ndarray) -> None:
+        """Deletes the `Applications/[slice]` subfolder
 
-        :param model: The model for which the folder structure will be deleted
         :param slice: The slice for which the folder structure will be deleted
         """
-        path = ResultManager.getSlicePath(model, slice)
+        path = ResultManager.getSlicePath(slice)
         shutil.rmtree(path)
 
-    @staticmethod
-    def getApplicationPath(model) -> str:
+    def getApplicationPath(self) -> str:
         """Returns the path to the simulation results folder, containing also intermediate results
 
         :return: path as string to the simulation folder
         :rtype: str
         """
-        path = "Applications/" + model.name
+        path = "Applications/" + self.model_name
         return path
 
-    @staticmethod
-    def save_run(model, slice, run, samplerResults, finalWalkerPositions):
+    def save_run(
+        model: Model,
+        slice: np.ndarray,
+        run,
+        samplerResults: np.ndarray,
+        finalWalkerPositions: np.ndarray,
+    ):
         """Saves the results of a single run of the emcee particle swarm sampler.
         SamplerResults has the shape (numWalkers * numSteps, samplingDim + dataDim + 1), we save them
         as seperate files in the folders 'Params' and'SimResults' and 'densityEvals'.
@@ -162,7 +169,7 @@ class ResultManager:
 
         samplingDim = finalWalkerPositions.shape[1]
 
-        results_path = ResultManager.getSlicePath(model, slice)
+        results_path = ResultManager.getSlicePath(slice)
 
         # Save the parameters
         np.savetxt(
@@ -192,39 +199,34 @@ class ResultManager:
             delimiter=",",
         )
 
-    @staticmethod
     def save_overall(
-        model, slice, overallParams, overallSimResults, overallDensityEvals
+        slice, overallParams, overallSimResults, overallDensityEvals
     ):
         """Saves the results of all runs of the emcee particle swarm sampler for the given slice.
 
-        :param model: The model for which the results will be saved
         :param slice: The slice for which the results will be saved # TODO document dimensions of overallParams, overallSimResults, overallDensityEvals
         """
         # Save the three just-created files.
         np.savetxt(
-            ResultManager.getSlicePath() + "/OverallDensityEvals.csv",
+            ResultManager.getSlicePath(slice) + "/OverallDensityEvals.csv",
             overallDensityEvals,
             delimiter=",",
         )
         np.savetxt(
-            ResultManager.getSlicePath() + "/OverallSimResults.csv",
+            ResultManager.getSlicePath(slice) + "/OverallSimResults.csv",
             overallSimResults,
             delimiter=",",
         )
         np.savetxt(
-            ResultManager.getSlicePath() + "/OverallParams.csv",
+            ResultManager.getSlicePath(slice) + "/OverallParams.csv",
             overallParams,
             delimiter=",",
         )
 
-    @staticmethod
-    def loadSimResults(model, slice, numBurnSamples: int, occurrence: int):
+    def loadSimResults(slice, numBurnSamples: int, occurrence: int):
         """Load the files generated by the EPI algorithm through sampling
 
-        :param model: Model from which the results will be loaded
         :param slice: Slice for which the results will be loaded
-        :type model: Model
         :param numBurnSamples: Ignore the first samples of each chain
         :type numBurnSamples: int
         :param occurrence: step of sampling from chains
@@ -232,7 +234,7 @@ class ResultManager:
         :return: _description_
         :rtype: _type_
         """
-        results_path = ResultManager.getSlicePath(model, slice)
+        results_path = ResultManager.getSlicePath(slice)
 
         densityEvals = np.loadtxt(
             results_path + "/OverallDensityEvals.csv",
