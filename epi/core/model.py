cff54c810bea5b2bcd31fd7f83d2d349565aa4cc
Lars Kaiser <lars.g.kaiser@gmx.de>
2023-03-07 21:29:21 +0100

    Correct model docstrings

--- epi/core/model.py
+++ epi/core/model.py
@@ -12,15 +12,7 @@ from epi.jax_extension import value_and_jacrev
 
 
 class Model(ABC):
-    """The base class for all models using the EPI algorithm.
-
-    It contains three abstract methods which need to be implemented by subclasses
-
-    Args:
-
-    Returns:
-
-    """
+    """The base class for all models using the EPI algorithm."""
 
     paramDim = None
     dataDim = None
@@ -29,6 +21,7 @@ class Model(ABC):
     defaultCentralParam = None
 
     def __init_subclass__(cls, **kwargs):
+        """Check if the required attributes are set."""
         if not inspect.isabstract(cls):
             for required in (
                 "paramDim",
@@ -46,17 +39,13 @@ class Model(ABC):
         paramLimits: np.ndarray = None,
         name: str = None,
     ) -> None:
-        # Define model-specific lower and upper limits for the sampling
-        # to avoid parameter regions where the evaluation of the model is instable.
-        # The limits in the format np.array([lower_dim1, upper_dim1], [lower_dim2, upper_dim2], ...)
+        """Initializes the model with the given parameters.
 
-        # Define a model-specific central parameter point, which will be used as starting point for the mcmc sampler.
-        # A single parameter point in the format np.array([p_dim1, p_dim2, ...])
-
-        # Returns the name of the class to which the object belongs. Overwrite it if you want to
-        # give your model a custom name, e. g. depending on the name of your parameters.
-
-        # :return: The class name of the calling object.
+        Args:
+            centralParam(np.ndarray): The central parameter for the model. (Default value = None)
+            paramLimits(np.ndarray): The limits for the parameters. The limits are given as a 2D array with shape (paramDim, 2). (Default value = None)
+            name(str): The name of the model. The class name is used if no name is given. (Default value = None)
+        """
 
         assert centralParam is not None or self.defaultCentralParam is not None
         assert (
@@ -77,56 +66,52 @@ class Model(ABC):
             self.name = name
 
     @abstractmethod
-    def forward(self, param: np.ndarray):
+    def forward(self, param: np.ndarray) -> np.ndarray:
         """Executed the forward pass of the model to obtain data from a parameter. You can also do equivalently :code:`model(param)`.
 
         Args:
-          param: The parameter(set) for which the model should be evaluated.
-          param: np.ndarray:
+            param(np.ndarray): The parameter for which the data should be generated.
 
         Returns:
-
-        Raises:
-          NotImplementedError: Implement this method to make you model callable.
+            np.ndarray: The data generated from the parameter.
 
         """
         raise NotImplementedError
 
     @abstractmethod
-    def jacobian(self, param: np.ndarray):
+    def jacobian(self, param: np.ndarray) -> np.ndarray:
         """Evaluates the jacobian of the :func:`~epic.core.model.Model.forward` method.
 
         Args:
-          param(np.ndarray): The parameter(set) for which the jacobian of your model should be evaluated.
-          param: np.ndarray:
+            param(np.ndarray): The parameter for which the jacobian should be evaluated.
 
         Returns:
-          np.ndarray: The jacobian for the variables returned by the :func:`~epic.core.model.Model.forward` method with respect to the parameters.
+            np.ndarray: The jacobian for the variables returned by the :func:`~epic.core.model.Model.forward` method with respect to the parameters.
 
         """
         raise NotImplementedError
 
-    def valjac(self, param: np.ndarray):
+    def valjac(
+        self, param: np.ndarray
+    ) -> typing.Tuple[np.ndarray, np.ndarray]:
         """Evaluates the jacobian and the forward pass of the model at the same time. If the method is not overwritten in a subclass it,
         it simply calls :func:`~epic.core.model.Model.forward` and :func:`~epic.core.model.Model.jacobian`.
 
         Args:
-          param(np.ndarray): The parameter(set) for which the model and the jacobian should be evaluated.
-          param: np.ndarray:
+            param(np.ndarray): The parameter for which the jacobian should be evaluated.
 
         Returns:
+            typing.Tuple[np.ndarray, np.ndarray]: The data generated from the parameter and the jacobian for the variables returned by the :func:`~epic.core.model.Model.forward` method with respect to the parameters.
 
         """
 
         return self.forward(param), self.jacobian(param)
 
     def isArtificial(self) -> bool:
-        """Determines whether the model provides artificial data
-
-        Args:
+        """Determines whether the model provides artificial parameter and data sets.
 
         Returns:
-          bool: True if the model inherits from the ArtificialModelInterface
+            bool: True if the model inherits from the ArtificialModelInterface
 
         """
         return issubclass(self.__class__, ArtificialModelInterface)
@@ -137,26 +122,20 @@ class ArtificialModelInterface(ABC):
     and the corresponding artificial data dataset, which can be used to compare the results from epi with the ground truth.
     The comparison can be done using the plotEmceeResults.
 
-    Args:
-
-    Returns:
-
-    Raises:
-      NotImplementedError: Implement the generateArtificialData function to implement this interface.
-
     """
 
     @abstractmethod
-    def generateArtificialParams(self, numSamples: int):
+    def generateArtificialParams(self, numSamples: int) -> np.ndarray:
         """This method must be overwritten an return an numpy array of numSamples parameters.
 
         Args:
-          numSamples: int:
+            numSamples(int): The number of parameters to generate.
 
         Returns:
+            np.ndarray: The generated parameters.
 
         Raises:
-          NotImplementedError: _description_
+            NotImplementedError: If the method is not overwritten in a subclass.
 
         """
         raise NotImplementedError
@@ -164,17 +143,17 @@ class ArtificialModelInterface(ABC):
     def generateArtificialData(
         self,
         params: typing.Union[os.PathLike, str, np.ndarray],
-    ) -> None:
+    ) -> np.ndarray:
         """This method is called when the user wants to generate artificial data from the model.
 
         Args:
-          numSamples(int): The number of samples to generate.
-          params(np.ndarray or Path or str): The parameters for which the artificial data should be generated.
-          params: typing.Union[os.PathLike:
-          str:
-          np.ndarray]:
+            params: typing.Union[os.PathLike, str, np.ndarray]: The parameters for which the data should be generated. Can be either a path to a file, a numpy array or a string.
 
         Returns:
+            np.ndarray: The data generated from the parameters.
+
+        Raises:
+            TypeError: If the params argument is not a path to a file, a numpy array or a string.
 
         """
         if isinstance(params, str) or isinstance(params, os.PathLike):
@@ -201,11 +180,14 @@ class ArtificialModelInterface(ABC):
 
 def autodiff(_cls):
     """
+    Decorator to automatically create the jacobian method based on the forward method.
+    Additionally it jit compiles the forward and jacobian method with jax.
 
     Args:
-      _cls:
+        _cls: The class to decorate.
 
     Returns:
+        The decorated class with the jacobian method and the forward and jacobian method jit compiled with jax.
 
     """
     _cls.initFwAndBw()
@@ -219,61 +201,67 @@ class JaxModel(Model):
     Dont overwrite the __init__ method of JaxModel without calling the super constructor.
     Else your forward method wont be jitted.
 
-    Args:
-      Model(Model: Model): Abstract parent class
-
-    Returns:
-
     """
 
     def __init__(self, name: str = None) -> None:
+        """Constructor of the JaxModel class.
+
+        Args:
+            name: str: The name of the model. If None the name of the class is used.
+        """
         super().__init__(name=name)
         # TODO: Check performance implications of not setting this at the class level but for each instance.
         type(self).forward = partial(JaxModel.forward_method, self)
 
     def __init_subclass__(cls, **kwargs):
+        """Automatically create the jacobian method based on the forward method for the subclass."""
         return autodiff(super().__init_subclass__(**kwargs))
 
     @classmethod
     def initFwAndBw(cls):
-        """ """
-        # Calculate jitted methods for the subclass(es)
-        # It is an unintended sideeffect that this happens for all intermediate classes also.
-        # E.g. for: class CoronaArtificial(Corona)
+        """Calculates the jitted methods for the subclass(es).
+        It is an unintended sideeffect that this happens for all intermediate classes also.
+        E.g. for: class CoronaArtificial(Corona)
+        """
         cls.fw = jit(cls.forward)
         cls.bw = jit(jacrev(cls.forward))
         cls.vj = jit(value_and_jacrev(cls.forward))
 
     @staticmethod
-    def forward_method(self, param):
-        """
+    def forward_method(self, param: np.ndarray) -> np.ndarray:
+        """This method is called by the jitted forward method. It is not intended to be called directly.
 
         Args:
-          param:
+            param(np.ndarray): The parameter for which the data should be generated.
 
         Returns:
+            np.ndarray: The data generated from the parameter.
 
         """
         return type(self).fw(param)
 
-    def jacobian(self, param):
-        """
+    def jacobian(self, param: np.ndarray) -> np.ndarray:
+        """Jacobian of the forward pass with respect to the parameters.
 
         Args:
-          param:
+            param(np.ndarray): The parameter for which the jacobian should be evaluated.
 
         Returns:
+            np.ndarray: The jacobian for the variables returned by the :func:`~epic.core.model.Model.forward` method with respect to the parameters.
 
         """
         return type(self).bw(param)
 
-    def valjac(self, param):
-        """
+    def valjac(
+        self, param: np.ndarray
+    ) -> typing.Tuple[np.ndarray, np.ndarray]:
+        """Evaluates the jacobian and the forward pass of the model at the same time. This can be more efficient than calling the :func:`~epic.core.model.Model.forward` and :func:`~epic.core.model.Model.jacobian` methods separately.
 
         Args:
-          param:
+            param(np.ndarray): The parameter for which the jacobian should be evaluated.
 
         Returns:
+            typing.Tuple[np.ndarray, np.ndarray]: The data and the jacobian for a given parameter.
 
         """
         return type(self).vj(param)
