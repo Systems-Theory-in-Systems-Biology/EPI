021a94da1966f0788f8e2bbfb90f20ebef412520
deckard007 <63878559+castellinilinguini@users.noreply.github.com>
2023-04-26 00:13:07 +0200

    Fix bug with one-dimensional data spaces

--- eulerpi/core/data_transformation.py
+++ eulerpi/core/data_transformation.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import numpy as np
 
 
@@ -15,18 +17,24 @@ class DataTransformation:
             data (np.ndarray): The data to be normalized. Columns correspond to different dimensions. Rows correspond to different observations.
         """
         self.mean_vector = np.mean(data, axis=0)
-        self.normalizing_matrix = np.linalg.inv(
-            np.linalg.cholesky(np.cov(data, rowvar=False))
-        )  # TODO check in Silverman if this makes sense
-        self.determinant = np.linalg.det(self.normalizing_matrix)
+        if data.shape[1] == 1:
+            self.normalizing_matrix = 1 / np.std(data)
+            self.determinant = self.normalizing_matrix
+        else:
+            self.normalizing_matrix = np.linalg.inv(
+                np.linalg.cholesky(np.cov(data, rowvar=False))
+            )  # TODO check in Silverman if this makes sense
+            self.determinant = np.linalg.det(self.normalizing_matrix)
 
-    def normalize(self, data: np.ndarray) -> np.ndarray:
+    def normalize(self, data: Union[float, np.ndarray]) -> np.ndarray:
         """Normalize the given data.
 
         Args:
-            data (np.ndarray): The data to be normalized. Columns correspond to different dimensions. Rows correspond to different observations.
+            data (Union[float, np.ndarray]): The data to be normalized. Columns correspond to different dimensions. Rows correspond to different observations.
 
         Returns:
             np.ndarray: The normalized data.
         """
-        return np.matmul(data - self.mean_vector, self.normalizing_matrix)
+        if data is np.ndarray:
+            return np.matmul(data - self.mean_vector, self.normalizing_matrix)
+        return (data - self.mean_vector) * self.normalizing_matrix
