169cad7c22fdd0c068febc6ed856460fa8e0ba3d
deckard007 <63878559+castellinilinguini@users.noreply.github.com>
2023-04-26 23:43:24 +0200

    Add jax to accelerate normalization

--- eulerpi/core/data_transformation.py
+++ eulerpi/core/data_transformation.py
@@ -1,48 +1,85 @@
 from typing import Optional, Union
 
-import numpy as np
+import jax.numpy as jnp
+from jax import jit, tree_util
 
 
 class DataTransformation:
     """Class for normalizing data. The data is normalized by subtracting the mean and multiplying by the inverse of the Cholesky decomposition of the covariance matrix."""
 
-    normalizing_matrix: Union[np.ndarray, float]
-    mean_vector: Union[np.ndarray, float]
-    determinant: float
+    normalizing_matrix: Union[jnp.ndarray, jnp.double]
+    mean_vector: Union[jnp.ndarray, jnp.double]
+    determinant: jnp.double
 
-    def __init__(self, data: Optional[np.ndarray] = None) -> None:
+    def __init__(
+        self,
+        data: Optional[jnp.ndarray] = None,
+        normalizing_matrix: Optional[jnp.ndarray] = None,
+        mean_vector: Optional[jnp.ndarray] = None,
+        determinant: Optional[jnp.double] = None,
+    ) -> None:
         """Initialize the DataTransformation object by calculating the mean vector and the normalizing matrix.
 
         Args:
-            data (np.ndarray): The data to be normalized. Columns correspond to different dimensions. Rows correspond to different observations.
+            data (jnp.ndarray): The data to be normalized. Columns correspond to different dimensions. Rows correspond to different observations.
         """
-        self.mean_vector = np.mean(data, axis=0)
-        if data.shape[1] == 1:
-            self.normalizing_matrix = 1 / np.std(data)
-            self.determinant = self.normalizing_matrix
-        else:
-            self.normalizing_matrix = np.linalg.inv(
-                np.linalg.cholesky(np.cov(data, rowvar=False))
-            )  # TODO check in Silverman if this makes sense
-            self.determinant = np.linalg.det(self.normalizing_matrix)
+        if mean_vector is None:
+            mean_vector = jnp.mean(data, axis=0)
+        self.mean_vector = mean_vector
 
+        if normalizing_matrix is None:
+            if data.shape[1] == 1:
+                normalizing_matrix = 1 / jnp.std(data)
+                determinant = normalizing_matrix
+            else:
+                normalizing_matrix = jnp.linalg.inv(
+                    jnp.linalg.cholesky(jnp.cov(data, rowvar=False))
+                )  # TODO check in Silverman if this makes sense
+                determinant = jnp.linalg.det(normalizing_matrix)
+        self.normalizing_matrix = normalizing_matrix
+        self.determinant = determinant
+
+    @jit
     def normalize(
-        self, data: Union[float, np.ndarray]
-    ) -> Union[float, np.ndarray]:
+        self, data: Union[jnp.double, jnp.ndarray]
+    ) -> Union[jnp.double, jnp.ndarray]:
         """Normalize the given data.
 
         Args:
-            data (Union[float, np.ndarray]): The data to be normalized. Columns correspond to different dimensions. Rows correspond to different observations.
+            data (Union[jnp.double, jnp.ndarray]): The data to be normalized. Columns correspond to different dimensions. Rows correspond to different observations.
 
         Returns:
-            Union[float, np.ndarray]: The normalized data.
+            Union[jnp.double, jnp.ndarray]: The normalized data.
         """
-        if isinstance(self.normalizing_matrix, np.ndarray):
+        if isinstance(self.normalizing_matrix, jnp.ndarray):
             if self.normalizing_matrix.ndim > 1:
-                return np.transpose(
-                    np.matmul(
+                return jnp.transpose(
+                    jnp.matmul(
                         self.normalizing_matrix,
-                        np.transpose(data - self.mean_vector),
+                        jnp.transpose(data - self.mean_vector),
                     )
                 )
         return self.normalizing_matrix * (data - self.mean_vector)
+
+    def _tree_flatten(self):
+        """This function is used by JAX to flatten the object for JIT compilation."""
+        children = ()  # arrays / dynamic values
+        aux_data = {
+            "normalizing_matrix": self.normalizing_matrix,
+            "mean_vector": self.mean_vector,
+            "determinant": self.determinant,
+        }  # static values
+
+        return (children, aux_data)
+
+    @classmethod
+    def _tree_unflatten(cls, aux_data, children):
+        """This function is used by JAX to unflatten the object for JIT compilation."""
+        return cls(*children, **aux_data)
+
+
+tree_util.register_pytree_node(
+    DataTransformation,
+    DataTransformation._tree_flatten,
+    DataTransformation._tree_unflatten,
+)
