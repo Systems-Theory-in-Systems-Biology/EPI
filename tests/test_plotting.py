a0d790f948ec1bbb93c3fa4a10a2025c7e97f1ed
dervi <vincent.wagner@web.de>
2023-11-20 16:34:30 +0100

    Frist version of plotting test. (not functional)

---
+++ tests/test_plotting.py
@@ -0,0 +1,49 @@
+"""
+Test the plotting of samples using the COVID model
+"""
+
+import numpy as np
+
+from eulerpi.core.inference import InferenceType, inference
+from eulerpi.core.plotting import sample_violin_plot
+from eulerpi.examples.corona import Corona
+
+
+def test_sample_plotting():
+    """ """
+    model = Corona()
+
+    data = np.loadtxt("Data/Corona/true_data_sample.csv", delimiter=",")
+
+    num_walkers = 10
+    num_steps = 100
+    num_burn_in_samples = 10
+    thinning_factor = 1
+
+    run_name = "test_run"
+
+    inference(
+        model,
+        data=data,
+        inference_type=InferenceType.MCMC,
+        slices=[np.arange(model.param_dim)],
+        run_name=run_name,
+        num_runs=1,
+        num_walkers=num_walkers,
+        num_steps=num_steps,
+        num_burn_in_samples=num_burn_in_samples,
+        thinning_factor=thinning_factor,
+    )
+
+    sample_violin_plot(
+        model,
+        run_name,
+        what_to_plot="param",
+        axis_labels=[r"$k_i$", r"$k_d", r"$k_r$"],
+    )
+    sample_violin_plot(
+        model,
+        run_name,
+        what_to_plot="data",
+        axis_labels=[r"1", r"2", r"5", r"15 weeks"],
+    )
