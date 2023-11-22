e3b83a46f829c26ccd1c8aa8de4a54fca3f203be
dervi <vincent.wagner@web.de>
2023-11-22 13:19:33 +0100

    Change input arguements to raference sample and get rid of artificial_bool

--- eulerpi/core/plotting.py
+++ eulerpi/core/plotting.py
@@ -4,8 +4,11 @@ This module provides a basic plotting functionality to visualise sampling result
 Uses burn_in and thinning accordinng to the simulation settings.
 """
 
-from typing import Optional
+import os
+import pathlib
+from typing import Optional, Union
 
+import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import axes
@@ -19,22 +22,22 @@ from eulerpi.core.result_manager import ResultManager
 
 def sample_violin_plot(
     model: Model,
+    reference_sample: Union[str, os.PathLike, np.ndarray] = None,
     run_name: str = "default_run",
     what_to_plot: str = "param",
-    artificial_bool: bool = False,
     credibility_level: float = 0.95,
     num_vertical_grid_points: int = 100,
     axis_labels: Optional[list[str]] = None,
 ) -> axes:
 
     """Creates an overview figure with one violin plot for each marginal distribution.
-       Can be used for parameters and simulation results and compares true and inferred values when possible.
+       Can be used for parameters and simulation results and compares reference (or true underlying) and inferred values when possible.
 
     Args:
         model(Model): The model describing the mapping from parameters to data.
+        reference_sample(Union[str, os.PathLike, np.ndarray]): Depending on what_to_plot, this is either the data sample used for the inference, or a reference sample of "true" parameter samples. If a string is given, it is assumed to be a path to a file containing the respective sample. (Default value = None)
         run_name(str): The name of the inference run. (Default value = "default_run")
         what_to_plot(str): Choose between "param" and "data" to respectively visualize either the model parameters or output. (Default value = "param")
-        artificial_bool(bool): Indicates whether there is true (reference) parameter information available. (Default value = "False")
         credibility_level(float): Defines the probability mass (between 0 and 1) that is included within each of the violin plots. Choose 1 if you do not wand any cut-off. (Default value = 0.95)
         num_vertical_grid_points(int): Defines the resolution of the vertical violin plots. Can be increased for smoother plots or decresed for faster runtime. (default value = 100)
         axis_labels(list[str], optional): The labels depicted on the ordinate of the plot. Its size needs to be identical with the dimensionality of the plotted distribution. (Default value = None)
@@ -47,10 +50,7 @@ def sample_violin_plot(
     plt.rcParams.update({"font.size": 16})
     plt.rcParams["mathtext.fontset"] = "dejavuserif"
 
-    if (what_to_plot == "param") and (not artificial_bool):
-        reference_available = False
-    else:
-        reference_available = True
+    reference_available = reference_sample is not None
 
     if what_to_plot == "param":
         dim = model.param_dim
@@ -71,6 +71,17 @@ def sample_violin_plot(
 
     color3 = np.array([45.0, 49.0, 66.0]) / 255.0
     color4 = np.array([255.0, 218.0, 174.0]) / 255.0
+
+    # Load data from file if necessary
+    if reference_available:
+        if isinstance(reference_sample, (str, os.PathLike, pathlib.Path)):
+            reference_sample = np.loadtxt(
+                reference_sample, delimiter=",", ndmin=2
+            )
+        elif not isinstance(reference_sample, (np.ndarray, jnp.ndarray)):
+            raise TypeError(
+                f"The data argument must be a path to a file or a numpy array. The argument passed was of type {type(reference_sample)}."
+            )
 
     # define the locations and extends of the violin plots on the abscissa
     unit_locations = np.linspace(1, 2 * dim - 1, dim) / (2.0 * dim)
@@ -94,29 +105,20 @@ def sample_violin_plot(
 
     # determine upper and lower plot bounds according to the credibility levels and what to plot
     if reference_available:
-        true_sample = np.loadtxt(
-            "Data/"
-            + model.name
-            + "/true_"
-            + str(what_to_plot)
-            + "_sample.csv",
-            delimiter=",",
-        )
-
-        percentile_sample = np.copy(true_sample)
+        percentile_sample = np.copy(reference_sample)
 
     else:
         percentile_sample = np.copy(reconstructed_sample)
 
-    data_upper_percentile = np.percentile(
+    upper_percentile = np.percentile(
         percentile_sample, 100.0 * credibility_level, axis=0
     )
-    data_lower_percentile = np.percentile(
+    lower_percentile = np.percentile(
         percentile_sample, 100.0 * (1 - credibility_level), axis=0
     )
 
-    max_val = np.amax(data_upper_percentile)
-    min_val = np.amin(data_lower_percentile)
+    max_val = np.amax(upper_percentile)
+    min_val = np.amin(lower_percentile)
 
     # create single figure with variable width
     fig, ax = plt.subplots(figsize=(2 * dim, 6))
@@ -150,53 +152,61 @@ def sample_violin_plot(
         # in case there is a reference for the plotted qunatity avaialbe
         if reference_available:
             # cast to 2d array
-            true_matrix = np.transpose(np.array([true_sample[:, i]]))
+            reference_matrix = np.transpose(np.array([reference_sample[:, i]]))
 
             # calculate kernel width for KDE
-            scales = calc_kernel_width(true_matrix)
+            scales = calc_kernel_width(reference_matrix)
 
             # evaluate KDEs over the grid
-            true_KDE = eval_kde_gauss(true_matrix, vertical_grid, scales)
+            reference_KDE = eval_kde_gauss(
+                reference_matrix, vertical_grid, scales
+            )
 
             # normalize the KDEs and caluculate their cumulative distribution
-            true_KDE_norm_cumsum = np.cumsum(true_KDE / np.sum(true_KDE))
+            reference_KDE_norm_cumsum = np.cumsum(
+                reference_KDE / np.sum(reference_KDE)
+            )
 
             # create boolean arrays to filter the KDEs for the specified credibility level
-            true_konfidence_index = (
-                true_KDE_norm_cumsum > (1 - credibility_level) / 2.0
+            reference_konfidence_index = (
+                reference_KDE_norm_cumsum > (1 - credibility_level) / 2.0
             ) & (
-                true_KDE_norm_cumsum
+                reference_KDE_norm_cumsum
                 < credibility_level + (1 - credibility_level) / 2.0
             )
 
             # calculate the maximum density of the KDEs and the corresponding incidence
-            max_density = np.amax(true_KDE)
-            max_density_argument = vertical_grid_1d[np.argmax(true_KDE)]
+            max_density = np.amax(reference_KDE)
+            max_density_argument = vertical_grid_1d[np.argmax(reference_KDE)]
 
             # calculate violin envelopes for reference and reconstruction
-            true_left_bound = (
-                -0.5 * envelope_width / max_density * true_KDE
+            reference_left_bound = (
+                -0.5 * envelope_width / max_density * reference_KDE
                 + unit_locations[i]
             )
-            true_right_bound = (
-                0.5 * envelope_width / max_density * true_KDE
+            reference_right_bound = (
+                0.5 * envelope_width / max_density * reference_KDE
                 + unit_locations[i]
             )
 
             # filter the violin envelopes for the specified credibility level
-            true_left_bound_konf = true_left_bound[true_konfidence_index]
-            true_right_bound_konf = true_right_bound[true_konfidence_index]
+            reference_left_bound_konf = reference_left_bound[
+                reference_konfidence_index
+            ]
+            reference_right_bound_konf = reference_right_bound[
+                reference_konfidence_index
+            ]
 
             # plot the filtered violin envelopes for the reference
             ax.plot(
-                true_left_bound_konf,
-                vertical_grid_1d[true_konfidence_index],
+                reference_left_bound_konf,
+                vertical_grid_1d[reference_konfidence_index],
                 linewidth=3.0,
                 color=colorOrig,
             )
             ax.plot(
-                true_right_bound_konf,
-                vertical_grid_1d[true_konfidence_index],
+                reference_right_bound_konf,
+                vertical_grid_1d[reference_konfidence_index],
                 linewidth=3.0,
                 color=colorOrig,
             )
@@ -204,10 +214,13 @@ def sample_violin_plot(
             # close the envelopes by connecting the last and the first point
             for j in [0, -1]:
                 ax.plot(
-                    [true_left_bound_konf[j], true_right_bound_konf[j]],
                     [
-                        vertical_grid_1d[true_konfidence_index][j],
-                        vertical_grid_1d[true_konfidence_index][j],
+                        reference_left_bound_konf[j],
+                        reference_right_bound_konf[j],
+                    ],
+                    [
+                        vertical_grid_1d[reference_konfidence_index][j],
+                        vertical_grid_1d[reference_konfidence_index][j],
                     ],
                     linewidth=3.0,
                     color=colorOrig,
@@ -216,9 +229,9 @@ def sample_violin_plot(
             # fill the violin envelopes
 
             ax.fill_betweenx(
-                vertical_grid_1d[true_konfidence_index],
-                true_left_bound_konf,
-                true_right_bound_konf,
+                vertical_grid_1d[reference_konfidence_index],
+                reference_left_bound_konf,
+                reference_right_bound_konf,
                 color=colorOrig,
                 label=r"$\Phi_\mathcal{" + variable_name + "}$"
                 if i == 0
@@ -228,9 +241,9 @@ def sample_violin_plot(
 
             # draw arrows to show the width of the violin envelopes
             ax.arrow(
-                np.amin(true_left_bound),
+                np.amin(reference_left_bound),
                 max_density_argument,
-                np.amax(true_right_bound) - np.amin(true_left_bound),
+                np.amax(reference_right_bound) - np.amin(reference_left_bound),
                 0,
                 length_includes_head=True,
                 color=color3,
@@ -240,9 +253,10 @@ def sample_violin_plot(
             )
 
             ax.arrow(
-                np.amax(true_right_bound),
+                np.amax(reference_right_bound),
                 max_density_argument,
-                -np.amax(true_right_bound) + np.amin(true_left_bound),
+                -np.amax(reference_right_bound)
+                + np.amin(reference_left_bound),
                 0,
                 length_includes_head=True,
                 color=color3,
