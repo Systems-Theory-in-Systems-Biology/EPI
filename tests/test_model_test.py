08aba74fafb3efaac9525eb77950a13456ba8439
dervi <vincent.wagner@web.de>
2023-11-23 14:09:32 +0100

    Fix import bug.

--- tests/test_model_test.py
+++ tests/test_model_test.py
@@ -3,8 +3,7 @@ Test the model testing function on the Temperature and Heat model.
 This especially comprises one model with one 1D input and one 1D output and one function call without default arguments.
 """
 
-from model_test import test_model
-
+from eulerpi.core.model_test import test_model
 from eulerpi.examples.heat import Heat
 from eulerpi.examples.temperature import Temperature
 
