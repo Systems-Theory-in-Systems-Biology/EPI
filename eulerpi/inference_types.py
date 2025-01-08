d6a2978d073876e36f18032afe766069c63b2cbb
castellinilinguini <b.castellaz@outlook.com>
2025-01-08 18:39:04 +0100

    Fix problem with duplicate InferenceTypes Update imports

--- eulerpi/inference_types.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class InferenceType(Enum):
-    """Available modes for the :py:func:`inference <eulerpi.inference.inference>` function."""
-
-    GRID = 0  #: The grid inference uses a grid to evaluate the joint distribution.
-    SAMPLING = 1  #: The SAMPLING / MCMC inference uses a Markov Chain Monte Carlo sampler to sample from the joint distribution.
