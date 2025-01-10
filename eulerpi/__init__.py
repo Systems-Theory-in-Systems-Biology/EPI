2bb6cc1e7d323dd6380813445858cc7b25de3859
castellinilinguini <b.castellaz@outlook.com>
2025-01-10 15:24:24 +0100

    Update top level imports

--- eulerpi/__init__.py
+++ eulerpi/__init__.py
@@ -3,6 +3,8 @@ from jax import config
 
 from .inference_engines.inference_type import InferenceType as InferenceType
 from .inference import inference as inference
+from .inference_engines import InferenceResult as InferenceResult
+from .load import load_inference_result as load_inference_result
 
 config.update("jax_enable_x64", True)
 
